4a69c8abb7e5ac7420c549197bbdb60bff9f0af8
wuxuewu <2284490459@qq.com>
2018-12-29 19:47:25 +0800

    12.29

--- dep/MysqlKu.d
+++ dep/MysqlKu.d
@@ -1,1 +1,2 @@
-/home/wuxuewu/datamysql/link_obj/MysqlKu.o /home/wuxuewu/datamysql/dep/MysqlKu.d:MysqlKu.cpp MysqlKu.hpp
+/home/wuxuewu/datamysql/link_obj/MysqlKu.o: MysqlKu.cpp MysqlKu.hpp quanju.hpp readn.hpp txt.o /home/wuxuewu/datamysql/dep/MysqlKu.d:g.hpp \
+/home/wuxuewu/datamysql/link_obj/ timeku.hpp ../proto/SSmsg.pb.h ../pro.o /home/wuxuewu/datamysql/dep/MysqlKu.d:/DBmsg.pb.h
