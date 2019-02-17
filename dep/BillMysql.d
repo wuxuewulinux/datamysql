8d72581a4aaf7b02616242ce5dba71ceb20ec23f
wuxuewu <2284490459@qq.com>
2019-02-18 00:10:03 +0800

    2.18

---
+++ dep/BillMysql.d
@@ -0,0 +1,2 @@
+/home/wuxuewu/datamysql/link_obj/BillMysql.o: BillMysql.cpp BillMysql.hpp quanju.hpp readn.hpp txt.o /home/wuxuewu/datamysql/dep/BillMysql.d:g.hpp \
+/home/wuxuewu/datamysql/link_obj/ timeku.hpp ../proto/SSmsg.pb.h ../pro.o /home/wuxuewu/datamysql/dep/BillMysql.d:/DBmsg.pb.h
--- dep/gateway.d
+++ dep/gateway.d
@@ -3,4 +3,4 @@
 /home/wuxuewu/datamysql/link_obj/ enterfunction.hpp quanju.hpp readn.hpp timeku.hpp ../pro.o /home/wuxuewu/datamysql/dep/gateway.d:/SSmsg.pb.h \
 /home/wuxuewu/datamysql/link_obj/ ../proto/DBmsg.pb.h socketku.hpp pthreadstart.hpp e.o /home/wuxuewu/datamysql/dep/gateway.d:ll_ku.hpp \
 /home/wuxuewu/datamysql/link_obj/ HandlerList.hpp Handler.hpp GameMysqlHandler.hpp ../pro.o /home/wuxuewu/datamysql/dep/gateway.d:/DBmsg.pb.h \
- MysqlKu.hpp
+ MysqlKu.hpp BillMysql.hpp
